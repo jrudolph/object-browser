150057835354096785fac928e8083136ed004057
Johannes Rudolph <johannes_rudolph@gmx.de>
2009-06-11 21:25:20 +0200

    make licensing clear

--- src/net/virtualvoid/android/browser/ItemFactory.java
+++ src/net/virtualvoid/android/browser/ItemFactory.java
@@ -1,3 +1,22 @@
+/*
+    This file is part of the object-browser.
+
+    The object-browser is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    The object-browser is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with the object-browser.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright 2009 Johannes Rudolph
+*/
+
 package net.virtualvoid.android.browser;
 
 import java.io.File;
@@ -577,6 +596,23 @@ public class ItemFactory {
                 @Override
                 protected Object originalValueAt(int pos) {
                     return array[pos];
+                }
+            };
+        }
+        else if (o instanceof List){
+            final List<?> list = (List<?>) o;
+            return new MappedArray(Object.class){
+                @Override
+                protected Object mappedValueAt(int pos) {
+                    return list.get(pos);
+                }
+                @Override
+                protected int numValues() {
+                    return list.size();
+                }
+                @Override
+                protected Object originalValueAt(int pos) {
+                    return list.get(pos);
                 }
             };
         }
